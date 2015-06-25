3a7de69582706c33fe33e4283c86e49a364457b6
Neospiro <Neospiro@users.noreply.github.com>
2015-06-25 17:33:12 +0200

    prompt
    prompt the username or password if not found in config.json

--- LWSS.js
+++ LWSS.js
@@ -1,12 +1,30 @@
 var LW = require('./LW.js');
+var prompt = require('prompt');
 var fs = require('fs');
-
-var dir = '.';
 
 var compilationErrorTexts = {};
 
-var config = JSON.parse( fs.readFileSync( 'config.json' ) );
-var dir = config.dir;
+function getConfig(callback){
+  var config = JSON.parse( fs.readFileSync( 'config.json' ) );
+  prompt.override = config;
+  prompt.message='[PROMPT]';
+  prompt.start();
+  if(config.login=='' || config.password=='')
+    console.log("[PROMPT] Ces paramêtres n'ont pas été trouvés dans config.json : ");
+  prompt.get([{name:'login', description:'Login',required: true},{name:'password',hidden:true, description:'Mot de passe',required: true}], function(err, result){
+    var c = {};
+    c.debug=false;
+    c.dir='.';
+    if(config.debug!==null)
+      c.debug=config.debug;
+    if(config.dir!==null)
+      c.dir=config.dir;
+    c.login=result.login;
+    c.password=result.password;
+    callback(c);
+  });
+  return config;
+}
 
 function getAiFileWatcher( ai_id, filename ){
   return function ( curr, prev ) {
@@ -32,11 +50,11 @@ function getAiFileWatcher( ai_id, filename ){
 }
 
 
-function getAiFileFunction( ai ){ return function(){         
+function getAiFileFunction( ai, farmerdir ){ return function(){         
     // Téléchargement du code d'une ia
     LW.ai.get( ai.id, function( dataAi ){
       var ai = dataAi.ai;
-      var file = dir + '/' + ai.name + '.ls' ;
+      var file = farmerdir + '/' + ai.name + '.ls' ;
       
       //Ecriture dans le fichier
       fs.writeFile( file, ai.code, function( err ) {
@@ -52,40 +70,47 @@ function getAiFileFunction( ai ){ return function(){
   };
 }
 
+function startSourceSync(config){
+  LW.login( config.login, config.password, function( dataFarmer ) {
+    
+    if( !dataFarmer.success ){
+      console.log( 'Connection echouée (le programme va se terminer.)' );
+      setTimeout(function(){ /*console.log('fin');*/},1000);
+    }
+    else{
+      var farmer = dataFarmer.farmer;
+   
+       LW.lang.get( 'java_compilation', function( dataLang ){
+         compilationErrorTexts = dataLang.lang ;
+       });
+   
+       var farmerdir= config.dir+'/'+farmer.name;
+   
+       try{
+         fs.mkdirSync(farmerdir);
+       }
+       catch(e){
+         if( e.code == 'EEXIST' )
+           console.log( '[ATTENTION] Les sources locales sont écrasées' );
+         else 
+            console.log( 'Erreur non gérée', e );
+       }
+   
+   
+      //* Recupération des ia
+      LW.ai.getList( function( dataAiList ){
+        var aisList = dataAiList.ais;
+        var timetowait = 0;
+        
+        for(var ai in aisList){
+          setTimeout( getAiFileFunction(aisList[ai], farmerdir), 250*timetowait++ );//setTimeOut
+        }
+      });
+    }
+  });
+}
 
-LW.login( config.login, config.password, function( dataFarmer ) {
-  
-  if( !dataFarmer.success ){
-    console.log( 'Connection echouée' );
-  }
-  else{
-    var farmer = dataFarmer.farmer;
- 
-     LW.lang.get( 'java_compilation', function( dataLang ){
-       compilationErrorTexts = dataLang.lang ;
-     });
- 
-     dir+= '/'+farmer.name;
- 
-     try{
-       fs.mkdirSync(dir);
-     }
-     catch(e){
-       if( e.code == 'EEXIST' )
-         console.log( '[ATTENTION] Les sources locales sont écrasées' );
-       else 
-          console.log( 'Erreur non gérée', e );
-     }
- 
- 
-    //* Recupération des ia
-    LW.ai.getList( function( dataAiList ){
-      var aisList = dataAiList.ais;
-      var timetowait = 0;
-      
-      for(var ai in aisList){
-        setTimeout( getAiFileFunction(aisList[ai]), 250*timetowait++ );//setTimeOut
-      }
-    });
-  }
+var config = getConfig(function(config){
+  startSourceSync(config);
+  //console.log(config);
 });
