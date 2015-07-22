554a65a17c5e9d8887f0e5a8002c0265c5723fb7
Neospiro <Neospiro@users.noreply.github.com>
2015-07-22 14:28:39 +0200

    installation si pas de dossier "node_modules"

--- LWSS.bat
+++ LWSS.bat
@@ -1,2 +1,11 @@
+@echo off
+IF EXIST .\node_modules goto launch
+echo Téléchargement des dépendances en cours...
+echo Il faudrat relancer le bat pour lancer le programme.
+npm install
+:launch
+echo LeekWars SYNC
+echo.
 node LWSS.js
+
 pause
